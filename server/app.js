20b25d74f3d39941b42bc2837f6458f6bdcb4dba
Dominik Burgdörfer <dominik.burgdoerfer@googlemail.com>
2012-08-06 19:32:50 +0200

    Added nano middleware.

--- server/app.js
+++ server/app.js
@@ -21,6 +21,9 @@ app.configure(function(){
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
+    app.use(function (req, res, next) {
+        req.nano = nano;
+    });
     app.use(app.router);
     app.use(express.static(path.join(__dirname, 'public')));
 });
